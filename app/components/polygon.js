656669b84ee8a8a4d4d3b8be6edcd4306c80af64
andy <chaochaoqi@gmail.com>
2019-04-21 21:27:15 -0700

    Updating for overwritten functions

--- app/components/polygon.js
+++ app/components/polygon.js
@@ -1,14 +1,24 @@
-import Component from '@ember/component';
+import Ember from 'ember';
 
-export default Component.extend({
-  mouseDown(event) {
-    console.log(JSON.stringify(event))
-    console.log('Mouse down');
-  },
-  mouseUp() {
-    console.log('Mouse up')
-  },
-  mouseMove() {
-    console.log('Mouse moving')
+export default Ember.Component.extend({
+  store: Ember.inject.service(),
+  actions: {
+    mouseDown: function() {
+      var store = this.get('store');
+      store.findRecord('point', 1).then(function(record) {
+        console.log('This is from the component: ' + record)
+      }, function(err) { console.log(err) });
+      console.log('Mouse down');
+    },
+    mouseUp: function() {
+      console.log('Mouse up');
+    },
+    mouseMove: function() {
+      var store = this.get('store');
+      store.findRecord('point', 1).then(function(record) {
+        console.log('This is from the component: ' + record)
+      }, function(err) { console.log(err) });
+      console.log('Mouse moving');
+    }
   }
 });
